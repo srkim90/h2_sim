c0ccdc3bc8662b31173573bbc47d8655c5e96a1b
srkim90 <shshrka001@gmail.com>
2018-10-25 10:50:42 +0900

    modified:   SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+now_pwd=`pwd`
+check=`ls -lthr $now_pwd/main.py | wc -l`
+pypy_tar="Pypy2-6.0.0.tar.gz"
+
+if [ "$check" != "1" ]; then
+    echo "Invalid current directory: PWD=${now_pwd}"
+    exit
+fi
 
 if [ -f "~/.bash_profile"  ]; then
     cp -f ~/.bash_profile ~/.bash_profile.bk
@@ -14,23 +22,27 @@ cp -rf ./env/bin ~/.
 cp -rf ./env/cfg ~/.
 cp -rf ./env/log ~/.
 
-if [ -d ".tmp" ]; then
-    rm -rf "./.tmp"
+if [ ! -d ".download" ]; then
+    mkdir .download
 fi
-mkdir .tmp
-cd .tmp
+cd .download
 echo "Pypy Downloading..."
-wget "https://doc-04-as-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/4ufaukfjni86k1bv8kfhu7i85kt8a09g/1540368000000/14436876053751962895/*/1HJe1NVbkdi8GbnCui1bFI_5m-ffBXZ_m?e=download" -O Pypy2-6.0.0.tar.gz
+pypy_cksum=`cksum ./${pypy_tar}  | cut -d ' ' -f 1`
+if [ "$pypy_cksum" != "2455333854" ]; then
+    wget "https://doc-04-as-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/4ufaukfjni86k1bv8kfhu7i85kt8a09g/1540368000000/14436876053751962895/*/1HJe1NVbkdi8GbnCui1bFI_5m-ffBXZ_m?e=download" -O $pypy_tar
+fi
 
-tar -zxvf ./Pypy2-6.0.0.tar.gz
+tar -zxvf ./$pypy_tar
 if [ ! -d "~/.util" ]; then
     mkdir ~/.util
     mkdir ~/.util/python
 fi
 mv Pypy2-6.0.0 ~/.util/python/.
 cd ..
-rm -rf ./.tmp
+#rm -rf ./.download
 
 cd ..
-mv h2_sim src
+if [ -d "h2_sim" ]; then
+    mv h2_sim src
+fi
 
